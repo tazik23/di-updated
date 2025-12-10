8603a8106a78ce6af18e335edf0f082a27589b1f
Vladimir Golubev <gor.prast@bk.ru>
2025-12-11 04:10:25 +0500

    feat: add tokenizer realization

---
+++ TagsCloud/TextProcessing/Tokenizers/Tokenizer.cs
@@ -0,0 +1,18 @@
+namespace TagsCloud.TextProcessing.Tokenizers;
+
+public class Tokenizer : ITokenizer
+{
+    private readonly string[] _separators;
+        
+    public Tokenizer(string[] separators)
+    {
+        _separators = separators.Length > 0 
+            ? separators 
+            : ["\r\n", "\n", "\r"];
+    }
+        
+    public IEnumerable<string> Tokenize(string text)
+    {
+        return text.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
